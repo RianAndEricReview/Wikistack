de19ed83e2e46a462a2faa1771b5a2bc369d2c56
Rian Halperin <rianhalperin@gmail.com>
2018-03-06 15:43:44 -0500

    create page & user models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,16 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/re-wikistack')
+
+const Page = db.define('page', {
+  title: { type: Sequelize.STRING, allowNull: false },
+  urlTitle: { type: Sequelize.STRING },
+  content: Sequelize.TEXT,
+  status: { type: Sequelize.ENUM('open', 'closed'), allowNull: false }
+})
+
+const User = db.define('user', {
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: false }
+})
+
+module.exports = {Page, User}
