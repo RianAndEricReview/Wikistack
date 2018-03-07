6534cb917f6795901cb4973335a31fad19a2c15a
Eric Garnett <egcodes@gmail.com>
2018-03-07 11:28:27 -0500

    post saves title and content to db

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const { Page, User } = require('../models')
 
 router.get('/', (req, res, next) => {
   res.redirect('/')
 })
 
 router.post('/', (req, res, next) => {
-  res.json(req.body)
+  let page = Page.build({
+    title: req.body.title,
+    content: req.body.content
+  })
+  page.save()
+  res.redirect('/')
 })
 
 router.get('/add', (req, res, next) => {
