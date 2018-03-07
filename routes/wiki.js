918aaa438fe18a174fdd6824cb83c4e605bca6bd
Eric Garnett <egcodes@gmail.com>
2018-03-07 10:59:15 -0500

    add wiki route skeletons

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+  res.send('<h1>Youve got the wikis</h1>')
+})
+
+router.post('/', (req, res, next) => {
+  res.send('You posted the wiki')
+})
+
+router.get('/add', (req, res, next) => {
+  res.send('<p>from here you can see our adding page</p>')
+})
+
 module.exports = router
