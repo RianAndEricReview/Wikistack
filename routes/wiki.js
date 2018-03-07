869509b65858f5b2c6e540bf0065475b8ca1434c
Rian Halperin <rianhalperin@gmail.com>
2018-03-07 12:46:20 -0500

    add GET route for new URLs

--- routes/wiki.js
+++ routes/wiki.js
@@ -13,11 +13,28 @@ router.post('/', (req, res, next) => {
     status: req.body.status,
   })
   page.save()
-  res.redirect('/')
+  .then((resolve) => {
+    // res.json(resolve)
+    res.redirect(`/wiki/${page.urlTitle}`)
+  })
+  .catch((err) => console.error(err))
 })
 
 router.get('/add', (req, res, next) => {
   res.render('addpage')
+})
+
+router.get('/:urlTitle', (req, res, next) => {
+  // res.send('this is the url: ' + req.params.urlTitle)
+  Page.findOne({
+    where: {
+      urlTitle: req.params.urlTitle
+    }
+  })
+  .then((foundPage) => {
+    res.json(foundPage)
+  })
+  .catch((err) => console.error(err))
 })
 
 module.exports = router
